d726c80c64a50eb3c6d4324ff4bb773fa1fe3a0e
hossam-khalaf <hossamhassan303089@gmail.com>
2022-10-28 00:36:25 +0700

    bg- color

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 export default function Home() {
 	return (
-		<div>
+		<div className='bg-[rgb(36,36,36)] text-white h-screen'>
 			<Head>
 				<title>
 					Mohamed Khalifa | Wordpress Developer | E-Commerce Developer
@@ -16,6 +17,9 @@ export default function Home() {
 
 			<Header />
 			{/* Hero */}
+			<section id='hero'>
+				<Hero />
+			</section>
 			{/* About */}
 			{/* Experience */}
 			{/* Skills */}
